0a5a1675197da20147ffb02400cf9a3afc040b45
MageLink <mage.link.zhezard@gmail.com>
2022-10-04 23:41:03 -0300

    Basic layout

---
+++ src/Context/GeneralContext/Provider.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+// Context
+import GeneralContext from ".";
+// Utilities
+import apiCall from "../../api/apiCall";
+
+const GeneralContextProvider = ({ children }) => {
+  const [test, setTest] = useState(true);
+
+  const exampleUseApiCall = async (userId) => {
+    try {
+      const data = await apiCall({ url: `http://insertApiAddress` });
+      // Insert setData(data)
+    } catch (e) {
+      alert("Un error ha ocurrido. Por favor actualice la página");
+    }
+  };
+
+  useEffect(() => {
+    //console.log(
+    //  "Una buen ejemplo para traer data desde una Api al momento de cargar la app, es usar el useEffect acá"
+    //);
+  }, []);
+
+  return <GeneralContext.Provider value={{ test, setTest }}>{children}</GeneralContext.Provider>;
+};
+
+export default GeneralContextProvider;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import { Footer, Header, HomeContent } from "../../Components/";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <HomeContent />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
