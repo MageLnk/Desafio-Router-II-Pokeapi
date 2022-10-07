fac84895dde1a4993f41ba6c12677fef69470dc4
MageLink <mage.link.zhezard@gmail.com>
2022-10-07 20:22:03 -0300

    Comenté todo el proceso de lógica para la paginación, ya que me faltan ingredientes para terminar eso. Seguiré con otras cosas y luego sigo con la paginación

--- src/Context/GeneralContext/Provider.js
+++ src/Context/GeneralContext/Provider.js
@@ -6,21 +6,41 @@ import apiCall from "../../api/apiCall";
 
 const GeneralContextProvider = ({ children }) => {
   const [data, setData] = useState("nada");
+  //const [offsetPokemonPageSearch, setOffsetPokemonPageSearch] = useState(0);
 
   const doSearchByUserInput = (inputData) => {
     console.log("Temporal Log, for future implement", inputData);
   };
 
+  //const validationPagination = (next, previus) => {
+  //  console.log("next", next, "previus", previus);
+  //};
+
   const getAllData = async () => {
-    console.log("Perfect");
     try {
-      const data = await apiCall({ url: `https://pokeapi.co/api/v2/pokemon?offset=0&limit=15` });
+      const data = await apiCall({
+        url: `https://pokeapi.co/api/v2/pokemon?offset=0&limit=15`,
+      });
       console.log(data);
       // Insert setData(data)
     } catch (e) {
       alert("Un error ha ocurrido. Por favor actualice la página");
     }
   };
+
+  //const getPokemonPagination = async (nextPagination, previusPagination) => {
+  //  //validationPagination(nextPagination, previusPagination);
+  //  // Temporal Logic for previus and all that
+  //  try {
+  //    const data = await apiCall({
+  //      url: `https://pokeapi.co/api/v2/pokemon?offset=${offsetPokemonPageSearch}&limit=15`,
+  //    });
+  //    console.log(data);
+  //    // Insert setData(data)
+  //  } catch (e) {
+  //    alert("Un error ha ocurrido. Por favor actualice la página");
+  //  }
+  //};
 
   useEffect(() => {
     getAllData();
--- src/pages/Pokemons/index.jsx
+++ src/pages/Pokemons/index.jsx
@@ -1,8 +1,14 @@
-import { useParams } from "react-router-dom";
-
+//import { useContext } from "react";
+//import { useParams } from "react-router-dom";
+// Context
+//import GeneralContext from "../../Context/GeneralContext";
+// App
 const Pokemons = () => {
-  const { page } = useParams();
-  console.log("????", page);
+  //const { page } = useParams();
+  //const { getPokemonPagination } = useContext(GeneralContext);
+  //useEffect(() => {
+  //  getPokemonPagination(page);
+  //}, [page]);
   return <div>Pokemons</div>;
 };
 
