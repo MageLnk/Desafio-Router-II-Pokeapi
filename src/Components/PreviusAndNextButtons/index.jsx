535c0100d8a07ec61a618f8b474a7631a306cee6
MageLink <mage.link.zhezard@gmail.com>
2022-10-09 15:46:20 -0300

    Buttons navigate through page with out a problem so far. Now, we need to make a responsive APi

--- src/Components/PreviusAndNextButtons/index.jsx
+++ src/Components/PreviusAndNextButtons/index.jsx
@@ -1,21 +1,53 @@
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 // Style
 import "./PreviusAndNextButtons.css";
 import { IconContext } from "react-icons";
 import { MdOutlineNavigateNext, MdNavigateBefore } from "react-icons/md";
-import { useParams } from "react-router-dom";
 // App
 const PreviusAndNextButtons = () => {
   const { page } = useParams();
-  console.log("ASDF", page);
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const validateParam = (params) => {
+    const numberValidation = isNaN(params);
+    if (!numberValidation === true) {
+      return +params;
+    }
+    if (!numberValidation === false) {
+      navigate("/error");
+    }
+  };
+
+  const handleClick = (path, params) => {
+    let validParam = validateParam(params);
+    if (pathname === "/") {
+      navigate("pokemons/2");
+      return;
+    }
+    if (path === "next") {
+      validParam = validParam + 1;
+      navigate(`/pokemons/${validParam.toString()}`);
+    }
+    if (path === "previous") {
+      validParam = validParam - 1;
+      navigate(`/pokemons/${validParam.toString()}`);
+    }
+  };
+
   return (
     <div className="previus-and-next-buttons-container">
       <IconContext.Provider value={{ className: "icons-buttons" }}>
-        <button>
+        <button
+          onClick={() => handleClick("previous", page)}
+          disabled={pathname === "/" || (page === "1" && true)}
+        >
           <span>
             <MdNavigateBefore /> Anterior
           </span>
         </button>
-        <button>
+
+        <button onClick={() => handleClick("next", page)} disabled={pathname === "/" && false}>
           <span>
             Siguiente <MdOutlineNavigateNext />
           </span>
